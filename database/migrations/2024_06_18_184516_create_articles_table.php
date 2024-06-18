8548d62a48c1afcbff2cedcaa64af47d89f92755
Michele Danza <michele-danza@hotmail.it>
2024-06-18 20:54:48 +0200

    Creazione della migration e del model dell' articolo

---
+++ database/migrations/2024_06_18_184516_create_articles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',255);
+            $table->unsignedInteger('price');
+            $table->text('description');
+            $table->unsignedTinyInteger('category_id');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
