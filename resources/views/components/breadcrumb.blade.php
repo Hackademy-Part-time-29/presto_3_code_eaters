82d22abe4486bb77021d795a46d7857822638cef
Marzio <marziofarina@icloud.com>
2024-06-26 12:44:03 +0200

    completata la breadcrumb con i vari articoli e categorie

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -5,6 +5,7 @@
             <li class="breadcrumb-item"><span>Articoli</span></li>
         @elseif (Route::is('byMacroCategory'))
             <li class="breadcrumb-item"><a href="/">Home</a></li>
+            <li class="breadcrumb-item"><a href="{{route('article.index')}}">Articoli</a></li>
             @foreach ($categories as $category)
                 @if ($category->macroCategory->id == request()->segment(2))
                     <li class="breadcrumb-item"><span>{{ $category->macroCategory->name }}</span></li>
@@ -12,7 +13,15 @@
                 @endif
             @endforeach            
         @elseif (Route::is('byCategory'))
-
+            <li class="breadcrumb-item"><a href="/">Home</a></li>
+            <li class="breadcrumb-item"><a href="{{route('article.index')}}">Articoli</a></li>
+            @foreach ($categories as $category)
+                @if ($category->id == request()->segment(2))
+                    <li class="breadcrumb-item"><a href="/MacroCategory/{{$category->macroCategory->id}}">{{ $category->macroCategory->name }}</a></li>
+                    <li class="breadcrumb-item"><span>{{ $category->name }}</span></li>
+                    @break
+                @endif
+            @endforeach            
         @else
 
         @endif
