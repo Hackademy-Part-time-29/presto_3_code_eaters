7ef1513bcf0d577b5c946c8c7728f15c5a8a9338
Marzio <marziofarina@icloud.com>
2024-06-27 22:40:52 +0200

    aggiunta la scomparsa dell'opzione rendi revisore qualora l'utente fosse già revisore

--- resources/views/components/new-footer.blade.php
+++ resources/views/components/new-footer.blade.php
@@ -3,62 +3,57 @@
         <div class="container">
             <div class="row">
                 @auth                    
-                <div class="col-lg-4 col-md-12 mb-5">
-                    <div class="f_widget company_widget wow fadeInLeft" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInLeft;">
-                        <h3 class="f-title f_600 t_color f_size_18">Vuoi diventare Revisore?</h3>
-                        <p>Cliccando il bottone sottostante farai richiesta al nostro admin</p>
-                            <a href="{{route('become.revisor')}}" class="btn btn_get btn_get_two button_form" type="submit">Diventa Revisore</a>
-                            <p class="mchimp-errmessage" style="display: none;"></p>
-                            <p class="mchimp-sucmessage" style="display: none;"></p>
-                        </form>
+                    <div class="col-lg-4 col-md-12 mb-5">
+                        @if (auth()->user()->is_revisor == false)
+                            <div class="f_widget company_widget wow fadeInLeft" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInLeft;">
+                                <h3 class="f-title f_600 t_color f_size_18">Vuoi diventare Revisore?</h3>
+                                <p>Cliccando il bottone sottostante farai richiesta al nostro admin</p>
+                                    <a href="{{route('become.revisor')}}" class="btn btn_get btn_get_two button_form" type="submit">Diventa Revisore</a>
+                                    <p class="mchimp-errmessage" style="display: none;"></p>
+                                    <p class="mchimp-sucmessage" style="display: none;"></p>
+                                </form>
+                            </div>
+                        @endif                            
                     </div>
-                </div>
-
-                <div class="col-lg-4 col-md-12 mb-5">
-                    <img class="img_footer" src="{{asset('storage/img/footer.png')}}">
-                </div>
-
-                <div class="col-lg-4 col-md-12 mb-5">
-                    <div class="f_widget about-widget pl_70 wow fadeInLeft" data-wow-delay="0.6s" style="visibility: visible; animation-delay: 0.6s; animation-name: fadeInLeft;">
-                        <h3 class="f-title f_600 t_color f_size_18">About us</h3>
-                        <ul class="list-unstyled f_list">
-                            <p><a class="a_form" href="#">FAQ</a></p>
-                            <li><a class="a_form" href="#">Term &amp; conditions</a></li>
-                            <li><a class="a_form" href="#">Reporting</a></li>
-                            <li><a class="a_form" href="#">Documentation</a></li>
-                            <li><a class="a_form" href="#">Support Policy</a></li>
-                            <li><a class="a_form" href="#">Privacy</a></li>
-                        </ul>
+                    <div class="col-lg-4 col-md-12 mb-5">
+                        <img class="img_footer" src="{{asset('storage/img/footer.png')}}">
                     </div>
-                </div>
-
+                    <div class="col-lg-4 col-md-12 mb-5">
+                        <div class="f_widget about-widget pl_70 wow fadeInLeft" data-wow-delay="0.6s" style="visibility: visible; animation-delay: 0.6s; animation-name: fadeInLeft;">
+                            <h3 class="f-title f_600 t_color f_size_18">About us</h3>
+                            <ul class="list-unstyled f_list">
+                                <p><a class="a_form" href="#">FAQ</a></p>
+                                <li><a class="a_form" href="#">Term &amp; conditions</a></li>
+                                <li><a class="a_form" href="#">Reporting</a></li>
+                                <li><a class="a_form" href="#">Documentation</a></li>
+                                <li><a class="a_form" href="#">Support Policy</a></li>
+                                <li><a class="a_form" href="#">Privacy</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 @else
-
-                <div class="col-lg-6 col-md-12 mb-5">
-                    <img class="img_footer" src="{{asset('storage/img/footer.png')}}">
-                </div>
-
-                <div class="col-lg-6 col-md-12 mb-5">
-                    <div class="f_widget about-widget pl_70 wow fadeInLeft" data-wow-delay="0.6s" style="visibility: visible; animation-delay: 0.6s; animation-name: fadeInLeft;">
-                        <h3 class="f-title f_600 t_color f_size_18">About us</h3>
-                        <ul class="list-unstyled f_list">
-                            <p><a class="a_form" href="#">FAQ</a></p>
-                            <li><a class="a_form" href="#">Term &amp; conditions</a></li>
-                            <li><a class="a_form" href="#">Reporting</a></li>
-                            <li><a class="a_form" href="#">Documentation</a></li>
-                            <li><a class="a_form" href="#">Support Policy</a></li>
-                            <li><a class="a_form" href="#">Privacy</a></li>
-                        </ul>
+                    <div class="col-lg-6 col-md-12 mb-5">
+                        <img class="img_footer" src="{{asset('storage/img/footer.png')}}">
                     </div>
-                </div>
-
+                    <div class="col-lg-6 col-md-12 mb-5">
+                        <div class="f_widget about-widget pl_70 wow fadeInLeft" data-wow-delay="0.6s" style="visibility: visible; animation-delay: 0.6s; animation-name: fadeInLeft;">
+                            <h3 class="f-title f_600 t_color f_size_18">About us</h3>
+                            <ul class="list-unstyled f_list">
+                                <p><a class="a_form" href="#">FAQ</a></p>
+                                <li><a class="a_form" href="#">Term &amp; conditions</a></li>
+                                <li><a class="a_form" href="#">Reporting</a></li>
+                                <li><a class="a_form" href="#">Documentation</a></li>
+                                <li><a class="a_form" href="#">Support Policy</a></li>
+                                <li><a class="a_form" href="#">Privacy</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 @endauth
             </div>
         </div>
         <div class="footer_bg">
             <div class="footer_bg_one"></div>
-            <div class="footer_bg_two"></div>
-            
+            <div class="footer_bg_two"></div>            
         </div>
     </div>
     <div class="footer_bottom">
