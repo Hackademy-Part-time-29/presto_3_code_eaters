bfea3ec995091d0f319ff1a5c475c463b91964d7
Marzio <marziofarina@icloud.com>
2024-06-27 18:38:28 +0200

    pull di marzio

--- resources/views/components/h-t-m-l.blade.php
+++ resources/views/components/h-t-m-l.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Document</title>
 </head>
