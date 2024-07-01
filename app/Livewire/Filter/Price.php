8e4eef09334a6d28ab6dabcd1290ac9a5b1ddb8d
Marzio <marziofarina@icloud.com>
2024-07-01 11:13:38 +0200

    creato il componente livewire filter/price

---
+++ app/Livewire/Filter/Price.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Filter;
+
+use Livewire\Component;
+
+class Price extends Component
+{
+    public function render()
+    {
+        return view('livewire.filter.price');
+    }
+}
--- resources/views/components/filtri.blade.php
+++ resources/views/components/filtri.blade.php
@@ -1,7 +1,7 @@
 <div class="col-3 contenitoreFiltri">
     <livewire:order/>
-     
-    <div class="contenitoreFiltroPerPrezzo">
+    <livewire:filter.price/>
+    <div>
         <p>Diagramma prezzi con chart.js</p>
         <p>range sottostante</p>
         <p>select valuta</p>
