a778518aca3fa3a7787ae989f3ac35368c9c44d4
Marzio <marziofarina@icloud.com>
2024-07-01 13:46:58 +0200

    ricavato il prezzo massimo degli articoli revisionati

--- app/Livewire/Filter/Price.php
+++ app/Livewire/Filter/Price.php
@@ -2,15 +2,25 @@
 
 namespace App\Livewire\Filter;
 
+use App\Models\Article;
 use Livewire\Component;
 
 class Price extends Component
 {
-    public $minPrice = 0;
-    public $maxPrice = 1000;
-    
+    public $price;
+
+    public function updatedPrice($value) {
+        $this->price = $value;
+    }
     public function render()
     {
+        $query = Article::where('is_accepted', true);
+        $this->price = $query->orderBy('price', 'desc')->first();
+
+        if ($this->price) {
+            $query->where('price', '<=', $this->price);
+        }
+        
         return view('livewire.filter.price');
     }
 }
--- resources/views/livewire/filter/price.blade.php
+++ resources/views/livewire/filter/price.blade.php
@@ -3,13 +3,16 @@
         <div>
             <span>Prezzo</span>
         </div>
-        <input type="range" class="form-range" id="customRange1" value="100">
+        <input type="range" class="form-range" id="customRange1" value={{$price->price}}>
         <div class="text-end">
-            <span>max</span>
+            <span>max : {{$price->price}}</span>
         </div>
     </div>
     <select class="form-select mt-1">
         <option>EUR • €</option>
         <option>USD • US$</option>
     </select>    
+    {{-- @foreach ($articles as $article)
+        <p>{{$article->price}}</p>
+    @endforeach --}}    
 </div>
