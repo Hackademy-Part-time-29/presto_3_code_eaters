f5467361014fc8b240ab24ac1d6012efda1dd806
Emanuele <emanu360@hotmail.it>
2024-07-02 14:33:07 +0200

    collegati titoli dei nostri servizi e tradotti completamente

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,22 +36,22 @@
     </section>
     <section class="bg-light py-5">
         <div class="container">
-            <h2 class="mb-4">I Nostri Servizi</h2>
+            <h2 class="mb-4">{{__('ui.servizi')}}</h2>
             <div class="row">
                 <div class="col-md-4">
-                    <h3>Acquisti Personalizzati tramite AI </h3>
+                    <h3>{{__('ui.servizio1')}}</h3>
                     <p>Raccomandazioni Personalizzate: Prodotti suggeriti in base agli acquisti precedenti e agli interessi dell'utente offrendoti
                     Promozioni Mirate ed esperienze di acquisto curate
                     </p>
                 </div>
                 <div class="col-md-4">
-                    <h3>Programmi di Abbonamento e Servizi Continuativi</h3>
+                    <h3>{{__('ui.servizio2')}}</h3>
                     <p>Consegne Regolari di Prodotti: Abbonamenti per prodotti di uso quotidiano, come caffè, articoli per la casa, o prodotti di bellezza, con consegne automatiche a intervalli prestabiliti.
                     Accesso a Contenuti Esclusivi: Contenuti premium, come tutorial, guide, e webinar, riservati agli abbonati per migliorare l'esperienza del prodotto.
                     Sconti e Offerte Esclusive: Promozioni speciali, sconti e accesso anticipato ai nuovi lanci di prodotto riservati ai membri del programma di abbonamento.</p>
                 </div>
                 <div class="col-md-4">
-                    <h3>Assistenza Clienti 24/7 tramite Chatbot e Live Chat</h3>
+                    <h3>{{__('ui.servizio3')}}</h3>
                     <p>Chatbot Avanzati: Utilizzo di chatbot alimentati dall'intelligenza artificiale per rispondere rapidamente alle domande frequenti, assistere nella navigazione del sito, e risolvere problemi comuni in qualsiasi momento del giorno o della notte.
 Live Chat con Operatori Umani: Accesso a rappresentanti del servizio clienti in tempo reale per risolvere problemi più complessi e fornire consigli personalizzati.</p>
                 </div>
