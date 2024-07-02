091fc1784c4ea8ded18ad1a2b77c0c304a6f99d8
Pasatino <andreipasatino@gmail.com>
2024-07-02 20:19:08 +0200

    aggiunto libreria spatie/image e creato il file jobs construttore

---
+++ app/Jobs/ResizeImage.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ResizeImage implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    private $w, $h, $fileName, $path;
+    public function __construct($filePath, $w, $h)
+    {
+        $this->path = dirname($filePath);
+        $this->fileName = basename($filePath);
+        $this->w = $w;
+        $this->h = $h;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        //
+    }
+}
