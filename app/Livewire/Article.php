3f9af5a4837288a30b15fa04680ccedd3a77f239
Pasatino <andreipasatino@gmail.com>
2024-07-03 18:39:22 +0200

    merge

--- app/Livewire/Article.php
+++ app/Livewire/Article.php
@@ -55,11 +55,9 @@ class Article extends Component{
 
         if (count($this->images) > 0) {
             foreach ($this->images as $image) {
-                $newFileName = "articles/{$this->article->id}";
-                $newImage = $this->article->images()->create(['path' => $image->store($newFileName, 'public')]);
-                dispatch(new ResizeImage($newImage->path, 300, 300));
+                $this->article->images()->create(['path' => $image->store('images', 'public')]);
             }
-            File::deleteDirectory(storage_path());
+        }
 
         session()->flash('success','Articolo creato con successo');
         $this->cleanForm();
