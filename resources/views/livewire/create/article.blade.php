603aa1ce704bad2c9017d640d601367a78c19d25
Marzio <marziofarina@icloud.com>
2024-07-03 21:50:03 +0200

    salvato il mio codice e caricato il codice con il pull

--- resources/views/livewire/create/article.blade.php
+++ resources/views/livewire/create/article.blade.php
@@ -55,7 +55,6 @@
                     </div>
                 </div>
             </div>
-            
         @endif
         <div class="d-flex justify-content-center">
             <button type="submit" class="btn btn-primary form--submit mt-5 footer_bottom">{{__('ui.creaarticolobtn')}}</button>
