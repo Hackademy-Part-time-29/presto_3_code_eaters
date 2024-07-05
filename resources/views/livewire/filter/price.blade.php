bf1729ab52d75a0a872223f133e7ec40bfe51d99
Marzio <marziofarina@icloud.com>
2024-07-05 18:21:04 +0200

    tolti i filtri

--- resources/views/livewire/filter/price.blade.php
+++ resources/views/livewire/filter/price.blade.php
@@ -1,7 +1,7 @@
 <div class="contenitoreOrdinamentoFiltri">
     <div class="contenitoreFiltroPerPrezzo mt-2 p-3">
         <div><span>Prezzo</span></div>
-        <input type="range" class="form-range" id="customRange1" min="0" max={{$price->price}} wire:model="price">
+        <input type="range" class="form-range" id="customRange1" value="{{$price->price}}" wire:model="price">
         <div class="text-end">
             <span>max : {{$price->price}}</span>
         </div>
