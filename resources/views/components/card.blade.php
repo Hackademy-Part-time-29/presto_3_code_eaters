22ee8660fa770e4ee5ba9d5491f9237c9e366fcf
Michele Danza <michele-danza@hotmail.it>
2024-07-05 11:21:38 +0200

    Creazione bottone acquista

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,11 +1,23 @@
 <div class="articleCard" id="container">	
-    <div class="product-details">
-      <a href="{{route('article.show',compact('article'))}}"><h1>{{Str::limit($article->title, 30, '...')}}</h1></a>
-      <span class="price">{{$article->price}}€</span>
-      <div>
-        <a href="{{route('byCategory', ['category' => $article->category])}}" class="information">{{Str::limit($article->category->name, 20, '...')}}</a>
+  <div class="product-details">
+    <a href="{{route('article.show',compact('article'))}}"><h1>{{Str::limit($article->title, 25, '...')}}</h1></a>
+    <p>{{Str::limit($article->description,50,'...')}}</p>
+    <span class="price">Prezzo: {{$article->price}}€</span>
+    <div>
+      <a href="{{route('byCategory', ['category' => $article->category])}}" class="information">{{Str::limit($article->category->name, 20, '...')}}</a>
+    </div>
+    <div  class="buttonBuy">
+      <div class="buttonBuy-wrapper">
+        
+        <span class="iconBuy">
+          <svg viewBox="0 0 16 16" class="bi bi-cart2" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
+          </svg>
+        </span>
+        <div class="textBuy">Acquista</div>
       </div>
-     
+    </div>
+    
   </div>
   
   <div class="product-image">
