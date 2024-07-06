184717eb27c09fa6b44f94c7c54e3a5d8172a7d4
Marzio <marziofarina@icloud.com>
2024-07-06 11:59:51 +0200

    modificata l'estetica dei filtri con la creazione e modifiche di classi css

--- resources/views/components/filtri.blade.php
+++ resources/views/components/filtri.blade.php
@@ -1,4 +1,4 @@
 <div class="col-12 col-sm-10 col-md-4 contenitoreFiltri">
     <livewire:order/>
-    {{-- <livewire:filter.price/>    --}}
+    <livewire:filter.price/>   
 </div>
--- resources/views/livewire/filter/price.blade.php
+++ resources/views/livewire/filter/price.blade.php
@@ -1,13 +1,15 @@
 <div class="contenitoreOrdinamentoFiltri">
-    <div class="contenitoreFiltroPerPrezzo mt-2 p-3">
-        <div><span>Prezzo</span></div>
-        <input type="range" class="form-range" id="customRange1" value="{{$price->price}}" wire:model="price">
-        <div class="text-end">
-            <span>max : {{$price->price}}</span>
+    <div class="contenitoreFiltroPerPrezzo">
+        <div class="contenitoreFiltroPerPrezzoDettaglio">
+            <div><span>Prezzo</span></div>
+            <input type="range" class="form-range" id="customRange1" value="{{$price->price}}" wire:model="price">
+            <div class="text-end">
+                <span>max : {{$price->price}}</span>
+            </div>
         </div>
-    </div>
-    <select class="form-select mt-1">
-        <option>EUR • €</option>
-        <option>USD • US$</option>
-    </select>    
+        <select class="form-select mt-1">
+            <option>EUR • €</option>
+            <option>USD • US$</option>
+        </select>    
+    </div>    
 </div>
