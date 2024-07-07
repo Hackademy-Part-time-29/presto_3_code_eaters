07f985c892ba7a0f75dfe75ceb298681519ef60d
Marzio <marziofarina@icloud.com>
2024-07-07 17:56:34 +0200

    rettificate le viste categorie e macrocategorie

--- resources/views/article/byCategory.blade.php
+++ resources/views/article/byCategory.blade.php
@@ -13,18 +13,16 @@
         <div class="row">
             <x-filtri/>
             <div class="col-12 col-md-8 mw-100 p-0">
-                <div class="d-flex flex-wrap">
-                    @forelse ($articles as $article)
-                        <x-card :article="$article" />
-                    @empty
-                        <div class="col-12 text-center">
-                            <h3>{{__('ui.noarticlecategory')}}</h3>
-                            @auth
-                                <a href="{{route('create.article')}}" class="btn btn-dark my-5">{{__('ui.creaarticolobtn')}}</a>
-                            @endauth
-                        </div>
-                    @endforelse
-                </div>           
+                @if(count($articles) > 0)
+                    <livewire:card/>
+                @else
+                    <div class="col-12 text-center">
+                        <h3>{{__('ui.noarticlecategory')}}</h3>
+                        @auth
+                            <a href="{{route('create.article')}}" class="btn btn-dark my-5">{{__('ui.creaarticolobtn')}}</a>
+                        @endauth
+                    </div>
+                @endif
             </div>
         </div>
     </div>
