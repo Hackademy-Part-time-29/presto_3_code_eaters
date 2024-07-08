31d965db34d45dd3f69f54f77a56f3ad17666c3e
Pasatino <andreipasatino@gmail.com>
2024-07-08 20:31:01 +0200

    aggiunto il job di google e modificato il file index.blade di revisor

---
+++ app/Jobs/GoogleVisionSafeSearch.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Image;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Google\Cloud\Vision\V1\ImageAnnotatorClient;
+
+class GoogleVisionSafeSearch implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    
+    private $article_image_id;
+    public function __construct($article_image_id){
+        $this->article_image_id = $article_image_id;
+    }
+    
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $i = Image::find($this->article_image_id);
+        if (!$i){
+            return;
+        }
+
+        $image = file_get_contents(storage_path('app/public/' . $i->path));
+        putenv('GOOGLE_APPLICATION_CREDENTIALS=' . base_path('google_credential.json'));
+
+        $imageAnnotator = new ImageAnnotatorClient();
+        $response = $imageAnnotator->safeSearchDetection($image);
+        $imageAnnotator->close();
+
+        $safe = $response->getSafeSearchAnnotation();
+
+
+        $adult = $safe->getAdult();
+        $medical = $safe->getMedical();
+        $spoof = $safe->getSpoof();
+        $violence = $safe->getViolence();
+        $racy = $safe->getRacy();
+
+        $likelihoodName = [
+            'text-secondary bi bi-circle-fill',
+            'text-success bi bi-check-circle-fill',
+            'text-success bi bi-check-circle-fill',
+            'text-success bi bi-exclamation-circle-fill',
+            'text-success bi bi-exclamation-circle-fill',
+            'text-danger bi bi-dash-circle-fill'
+
+        ];
+
+        $i->adult = $likelihoodName[$adult];
+        $i->spoof = $likelihoodName[$spoof];
+        $i->racy = $likelihoodName[$racy];
+        $i->medical = $likelihoodName[$medical];
+        $i->violence = $likelihoodName[$violence];
+
+        $i->save();
+    }
+}
--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -25,6 +25,48 @@
                                 <div class="col-6 col-md-4 mb-4 text-center">
                                     <img src="{{$image->getUrl(300,300)}}" alt="Immagine {{$key +1}} dell'articolo '{{$article_to_check->title}}" class="img-fluid rounded shadow">
                                 </div>
+
+                                <div class="col-md-8 ps-3">
+                                    <div class="card-body">
+                                        <h5 class="">Ratings</h5>
+                                        <div class="row justify-content-center">
+                                            <div class="col-2">
+                                                <div class="text-center mx-auto {{ $image->adult }}">
+                                                </div>
+                                            </div>
+                                            <div class="col-10">adult</div>
+                                        </div>
+                                        <div class="row justify-content-center">
+                                            <div class="col-2">
+                                                <div class="text-center mx-auto {{ $image->violence }}">
+                                                </div>
+                                            </div>
+                                            <div class="col-10">violence</div>
+                                        </div>
+                                        <div class="row justify-content-center">
+                                            <div class="col-2">
+                                                <div class="text-center mx-auto {{ $image->spoof }}">
+                                                </div>
+                                            </div>
+                                            <div class="col-10">spoof</div>
+                                        </div>
+                                        <div class="row justify-content-center">
+                                            <div class="col-2">
+                                                <div class="text-center mx-auto {{ $image->racy }}">
+                                                </div>
+                                            </div>
+                                            <div class="col-10">racy</div>
+                                        </div>
+                                        <div class="row justify-content-center">
+                                            <div class="col-2">
+                                                <div class="text-center mx-auto {{ $image->medical }}">
+                                                </div>
+                                            </div>
+                                            <div class="col-10">medical</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
                             @endforeach
                         @else
                             @for ($i = 0; $i < 6; $i++)
