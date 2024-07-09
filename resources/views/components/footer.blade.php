61bcdf1370331f6dba98d952b960d3818ab2e4b1
Emanuele <emanu360@hotmail.it>
2024-07-09 21:35:52 +0200

    provato miglioramento card invano

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -64,7 +64,7 @@
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-12">
-                    <p class="mb-0 f_400">© {{env('APP_NAME')}}.it All rights reserved.</p>
+                    <p class="mb-0 f_400">© {{env('APP_NAME')}} All rights reserved.</p>
                 </div>
             </div>
         </div>
