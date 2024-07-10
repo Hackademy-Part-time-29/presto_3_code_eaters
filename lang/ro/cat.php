9c7a302576a41b991a471bde31f4b94dfd1b1393
Emanuele <emanu360@hotmail.it>
2024-07-10 19:36:28 +0200

    sistemati colori google filter

--- lang/ro/cat.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'translations' => [
-        ['name' => 'suca', 'macroCategory_id' => '1'],
-        ['name' => 'Computer e Laptop', 'macroCategory_id' => '1'],
-        // Aggiungi qui tutte le altre categorie tradotte
-    ],
-];
