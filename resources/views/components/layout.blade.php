5587b38e82aaf74e1e7b62e137bfbcc13b32b86f
Marzio <marziofarina@icloud.com>
2024-07-11 15:55:35 +0200

    modifiche al layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -15,7 +15,7 @@
                 {{session('success')}}
             </div>
         @endsession
-        <div class="container-fluid vh-100">
+        <div class="container-fluid">
             <div class="row justify-content-center">
                 {{ $slot }}
             </div>
